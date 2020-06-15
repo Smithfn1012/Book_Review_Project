52a243a32710f958ea10c92931cc99787a8d0ce7
Smithfn1012 <58006468+Smithfn1012@users.noreply.github.com>
2020-06-15 14:03:47 -0500

    create reviews table

--- book_review_backend/book_review/db/schema.rb
+++ book_review_backend/book_review/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_15_184844) do
+ActiveRecord::Schema.define(version: 2020_06_15_190215) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,5 +34,15 @@ ActiveRecord::Schema.define(version: 2020_06_15_184844) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.integer "stars"
+    t.string "content"
+    t.bigint "book_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["book_id"], name: "index_reviews_on_book_id"
+  end
+
   add_foreign_key "books", "genres"
+  add_foreign_key "reviews", "books"
 end
